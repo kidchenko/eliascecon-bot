f01867b2c2945f21dc420291af7009cb50c0959c
kidchenko <kidchenko@gmail.com>
2016-07-01 23:44:33 -0300

    Create eliasBot class

---
+++ src/eliasBot.js
@@ -0,0 +1,73 @@
+import Bot from 'slackbots';
+import _ from 'underscore';
+
+export default class EliasBot {
+
+    constructor(settings) {
+        this.settings = settings;
+        
+
+        this.morningMessages = [
+        'BOM DIA SENHORES! COMO ESTAMOS?',
+        'BOM DIA! HOJE É DIA DE FAZER O CAFÉ PINGAR!',
+        'BOM DIA? OTIMO DIA! HOJE EU SINTO QUE O CAFÉ VAI PINGAR!',
+        'BOM DIA! JÁ FIZ TRÊS VENDAS HOJE! PAAAH!',
+        ]
+
+        this.howAreYouMessages = [
+        'SE VOCÊ ESTÁ BEM EU TAMBÉM ESTOU!',
+        ]
+
+        this.howAreWeMessages = [
+        'SE VOCÊS ESTÃO BEM EU TAMBÉM ESTOU!',
+        ]
+
+        this.triggers = {
+        'bom dia': this.morningMessages,
+        'estou otimo': this.howAreYouMessages,
+        'estou ótimo': this.howAreYouMessages,
+        'estamos otimos': this.howAreWeMessages,
+        'estamos ótimos': this.howAreWeMessages
+        };
+        this.bot = new Bot(this.settings);
+    }
+
+    run() {
+        console.log('run');
+        this.bot.on('start', this._onStart)
+        this.bot.on('onMessage', this._onMessage);
+    }
+
+    _onStart() {
+        this.postMessage(this.getRandomMessage(this.startMessages), 'random')
+        console.log('> Elias has been started!')
+    }
+
+    _onMessage(data) {
+        const eliasId = 'U10NFSVN3'
+        if (data.type === 'message' && data.user !== eliasId) {
+            if (this.isTriggerMessage(sanitize(data.text))) {
+                this.postMessage(this.getRandomMessage(this.triggers[this.sanitize(data.text)]), 'random');
+            }
+        }
+    }
+
+    postMessage(message, channel) {
+        this.bot.postMessageToChannel(channel, message, {
+            as_user: '@eliascecon'
+        });
+    }
+
+    isTriggerMessage(data) {
+        return _.contains(_.keys(triggers), this.sanitize(data));
+    }
+
+    getRandomMessage(messages) {
+        console.log('getrnam')
+        return messages[parseInt(Math.random() * messages.length)];
+    }
+
+    sanitize(message) {
+        return message.replace(new RegExp('[?.!]'), '').toLowerCase();
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,76 +1,15 @@
-import SlackBots from 'slackbots';
+import EliasBot from './eliasBot';
 import express from 'express';
 import http from 'http';
-import _ from 'underscore';
+
 var app = express();
 
-const elias = new SlackBots({
+const elias = new EliasBot({
   token: process.env.ELIASTOKEN,
   name: 'eliascecon'
 });
 
-const startMessages = [
-  'Cheguei senhores. COMO ESTAMOS?',
-  'Acabei de chegar da minha aula de PNL. Foi FUDIDO!',
-  'Olá SENHORES, como estamos? Se vocês estão bem, eu também estou!',
-  'Estou de volta! Estava trabalhando no meu gerador de energia infinita. FUDIDO!'
-]
-
-const morningMessages = [
-  'BOM DIA SENHORES! COMO ESTAMOS?',
-  'BOM DIA! HOJE É DIA DE FAZER O CAFÉ PINGAR!',
-  'BOM DIA? OTIMO DIA! HOJE EU SINTO QUE O CAFÉ VAI PINGAR!',
-  'BOM DIA! JÁ FIZ TRÊS VENDAS HOJE! PAAAH!',
-]
-
-const howAreYouMessages = [
-  'SE VOCÊ ESTÁ BEM EU TAMBÉM ESTOU!',
-]
-
-const howAreWeMessages = [
-  'SE VOCÊS ESTÃO BEM EU TAMBÉM ESTOU!',
-]
-
-const triggers = {
-  'bom dia': morningMessages,
-  'estou otimo': howAreYouMessages,
-  'estou ótimo': howAreYouMessages,
-  'estamos otimos': howAreWeMessages,
-  'estamos ótimos': howAreWeMessages
-};
-
-var postMessage = exports.postMessage = function(message, channel) {
-  elias.postMessageToChannel(channel, message, {
-    as_user: '@eliascecon'
-  });
-}
-
-var isTriggerMessage = exports.isTriggerMessage = function(data) {
-  return _.contains(_.keys(triggers), sanitize(data));
-}
-
-var getRandomMessage = exports.getRandomMessage = function(messages) {
-  return messages[parseInt(Math.random() * messages.length)];
-}
-
-var sanitize = exports.sanitize = function(message) {
-  return message.replace(new RegExp('[?.!]'), '').toLowerCase();
-}
-
-// Events
-elias.on('start', function() {
-  postMessage(getRandomMessage(startMessages), 'random')
-  console.log('> Elias has been started!')
-});
-
-elias.on('message', function(data) {
-  const eliasId = 'U10NFSVN3'
-  if (data.type === 'message' && data.user !== eliasId) {
-    if (isTriggerMessage(sanitize(data.text))) {
-      postMessage(getRandomMessage(triggers[sanitize(data.text)]), 'random');
-    }
-  }
-})
+elias.run();
 
 // For avoidong Heroku $PORT error
 app.set('port', (process.env.PORT || 5000));
